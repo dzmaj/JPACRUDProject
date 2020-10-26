7f793c7951c734392a352e4d7493084069325a8f
Daniel Majewski <d.majewski.3453@gmail.com>
2020-10-26 10:13:42 -0600

    updated to deploy WAR

--- BootPerson/src/main/java/com/skilldistillery/jpacrud/BootPersonApplication.java
+++ BootPerson/src/main/java/com/skilldistillery/jpacrud/BootPersonApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootPersonApplication {
+public class BootPersonApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootPersonApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootPersonApplication.class, args);
